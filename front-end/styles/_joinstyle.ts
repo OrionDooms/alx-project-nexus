8190f3ef5bcac160fe2332bed8d5abe1ae640912
Orion <Orion45@dd.gmail.com>
2025-09-22 23:40:32 +0200

    style Signin/login

---
+++ front-end/styles/_joinstyle.ts
@@ -0,0 +1,132 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 20,
+    backgroundColor: "#242834",
+  },
+  iconsection: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  titleTextGroup: {
+    marginTop: 20
+  },
+  titleText: {
+    fontWeight: 700,
+    fontSize: 39,
+    color: '#B6FF00FF',
+  },
+  subText: {
+    fontWeight: 400,
+    fontSize: 12,
+    color: '#ffffffff',
+  },
+  formGroup: {
+    marginTop: 44,
+    rowGap: 10
+  },
+  formLabel: {
+    fontSize: 18,
+    fontWeight: 400,
+    color: '#B6FF00FF',
+    marginBottom: 7
+  },
+  formControl: {
+    borderWidth: 2,
+    height: 50,
+    borderRadius: 10,
+    borderColor: '#b6ff00ff',
+    paddingHorizontal: 10
+  },
+  formPasswordControl: {
+    borderWidth: 2,
+    height: 50,
+    borderRadius: 10,
+    borderColor: '#b6ff00ff',
+    paddingHorizontal: 10,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  passwordControl: {
+    flex: 1
+  },
+  primaryButton: {
+    backgroundColor: '#B6FF00FF',
+    height: 53,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 25
+  },
+  buttonText: {
+    color: '242834',
+    fontSize: 17,
+    fontWeight: 300
+  },
+  secondaryButtonGroup: {
+    rowGap: 15,
+    marginTop: 15,
+  },
+  secondaryButton: {
+    height: 53,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    columnGap: 10,
+    borderWidth: 1,
+    borderColor: '#B6FF00FF'
+  },
+  secondaryButtonText: {
+    fontSize: 18,
+    fontWeight: 400,
+    color: '#FFFFFF',
+  },
+  signupgroup: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    position: 'absolute',
+    left: 77, 
+    right: 76,
+    bottom: 33
+  },
+  signupTitleText: {
+    fontSize: 18,
+    fontWeight: 400,
+    color: '#ffffffff',
+  },
+  signupSubTitleText: {
+    fontSize: 18,
+    fontWeight: 600,
+    color: '#7D53FF',
+  },
+  dividerGroup: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    columnGap: 10,
+    marginBottom: 25,
+    marginTop: 29,
+  },
+  divider: {
+    borderWidth: 1,
+    flex: 1,
+    borderColor: '#B6FF00FF',
+  },
+  dividerText: {
+    fontSize: 17,
+    fontWeight: 500,
+    fontVariant: ['small-caps'],
+    color: '#ffffffff'
+  },
+  forgotPasswordText: {
+    textAlign: 'right',
+    color: '#7D53FF',
+    marginTop: 9,
+  }
+});
+
+export { styles }
