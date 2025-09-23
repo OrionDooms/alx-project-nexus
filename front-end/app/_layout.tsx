8670c4116afc14eb4952c75e614f87f176a78cfd
Orion <Orion45@dd.gmail.com>
2025-09-23 03:19:44 +0200

    Routing Layer

--- front-end/app/_layout.tsx
+++ front-end/app/_layout.tsx
@@ -3,7 +3,8 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="/" />
+      <Stack.Screen name="index"/>
+      <Stack.Screen name="(home)/Post" />
       <Stack.Screen name="join" />
       <Stack.Screen name="signin" />
     </Stack>
