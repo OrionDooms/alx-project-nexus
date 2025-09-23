bf4204dae4ed1c1167a7c9005576e2506fd2de2e
Orion <Orion45@dd.gmail.com>
2025-09-23 03:27:01 +0200

    link images

--- front-end/constants/index.ts
+++ front-end/constants/index.ts
@@ -1,5 +1,5 @@
-const BACKGROUNDIMAGE = require('@/assets/images/hero-icon.png')
-const HEROLOGO = require('@/assets/images/logo.png')
+const BACKGROUNDIMAGE = require('@/assets/images/purple_background.jpg')
+const HEROLOGO = require('@/assets/images/Rabbit_logo.jpg')
 const HEROLOGOGREEN = require("@/assets/images/logo-green.png")
 const GOOGLELOGO = require('@/assets/images/google.png')
 const FACEBOOKLOGO = require('@/assets/images/facebook.png')
