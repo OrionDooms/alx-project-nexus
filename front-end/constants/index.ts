9c384fb98517233f3f91051125a28cb15ffb3a29
Orion <Orion45@dd.gmail.com>
2025-09-24 02:39:10 +0200

    Added Images to homepage and postpage.

--- front-end/constants/index.ts
+++ front-end/constants/index.ts
@@ -1,6 +1,6 @@
 const BACKGROUNDIMAGE = require('@/assets/images/purple_background.jpg')
-const HEROLOGO = require('@/assets/images/Rabbit_logo.jpg')
-const HEROLOGOGREEN = require("@/assets/images/logo-green.png")
+const HEROLOGO = require('@/assets/images/fish.png')
+const HEROLOGOGREEN = require("@/assets/images/sea.png")
 const GOOGLELOGO = require('@/assets/images/google.png')
 const FACEBOOKLOGO = require('@/assets/images/facebook.png')
 
