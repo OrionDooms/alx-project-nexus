4057d22373c52e1a1d325427b849ced58f5990dd
Orion <Orion45@dd.gmail.com>
2025-10-02 01:17:41 +0200

    add style to the login and the signup page

--- front-end/styles/_joinstyle.ts
+++ front-end/styles/_joinstyle.ts
@@ -1,10 +1,14 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
+  backgroundImage: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
   container: {
     flex: 1,
     paddingHorizontal: 20,
-    backgroundColor: "#242834",
   },
   iconsection: {
     flexDirection: 'row',
